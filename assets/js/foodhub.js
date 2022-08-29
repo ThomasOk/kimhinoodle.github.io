7acbd6e4beb1ab919cc4b09be7aa0cefe3cd13cb
Thomas <ok.thomas.s@gmail.com>
2022-08-29 17:47:29 +0200

    Updates august

--- assets/js/foodhub.js
+++ assets/js/foodhub.js
@@ -16,123 +16,128 @@ const productsGrid = document.querySelector('.products-grid');
 
 const menu = [
   {
-    title: 'Samoussa (Boeuf ou Légumes)',
+    title: 'Samoussa (2 pièces)',
     category: 'Entrées',
-    price: '5.30',
-    img: './assets/images/samoussas.jpg',
-    desc: `De friands beignets triangulaires croustillants garnis d'une farce de légumes, de viande de boeuf et d'épices.`,
+    price: '6.00',
+    img: './assets/images/samoussas2.png',
+    desc: `De friands beignets triangulaires croustillants garnis
+    d'une farce de légumes, de viande de boeuf et
+    d'épices. </br> Au choix: boeuf ou légumes.`,
   },
   {
-    title: 'Nems (Porc, Poulet, Crevettes ou Légumes) ',
+    title: 'Nems porc ou crevettes (2 pièces)',
     category: 'Entrées',
     price: '5.50',
-    img: './assets/images/nems.jpg',
-    desc: `D'authentiques rouleaux frits constitués d'une farce à base de viande, crevettes ou légumes, agrémentés de nouilles de riz et entourée d'une galette de riz.`,
+    img: './assets/images/Nems.png',
+    desc: `D'authentiques rouleaux frits constitués d'une farce
+    à base de viande, crevettes ou légumes, agrémentés
+    de nouilles de riz et entourée d'une galette de riz. </br>
+    Choix poulet ou légumes : + 0,50 €.`,
   },
   {
     title: 'Rouleaux de printemps',
     category: 'Entrées',
-    price: '5.00',
-    img: './assets/images/rouleaux-printemps.jpg',
+    price: '5.50',
+    img: './assets/images/rouleaux_printemps.png',
     desc: `Spécialités culinaires du Vietnam, choisissez nos délicieux rouleaux de printemps, se composant d'une garniture à base de poulet ou crevettes, de nouilles de riz, et de nombreux ingrédients connus pour leurs bienfaits (concombre, laitue), pour ajouter une note fraîche , légère et saine à votre repas.`,
   },
   {
-    title: 'Raviolis frits',
+    title: 'Raviolis frits (4 pièces)',
     category: 'Entrées',
-    price: '5.30',
-    img: './assets/images/ravioli2.jpg',
+    price: '5.50',
+    img: './assets/images/raviolis.png',
     desc: ` De savoureux raviolis constitués d'une pâte de farine de blé enveloppant une délicieuse farce à base de viande et champignons parfumés.`,
   },
   {
-    title: 'Tempura',
+    title: 'Tempura (3 pièces)',
     category: 'Entrées',
-    price: '5.30',
-    img: './assets/images/tempura.png',
+    price: '6.00',
+    img: './assets/images/tempuras.png',
     desc: ` Délicieux beignets de crevette à base de farine de blé.`,
   },
   {
-    title: 'Salade de vermicelle (Boeuf, poulet ou Crevettes)',
+    title: 'Salade de vermicelles',
     category: 'Entrées',
-    price: '5.00',
-    img: './assets/images/salade-vermicelle.jpg',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
+    price: '5.50',
+    img: './assets/images/salade_vermicelles.png',
+    desc: `Découvrez nos salades fraîches aux vermicelles de
+    riz pleines de saveurs grâce à la sauce et à ses
+    parfums asiatiques. Au choix : poulet ou crevettes.`,
   },
   {
     title: 'Soupe Phnom Penh',
     category: 'Plats',
-    price: '12.00',
+    price: '13.00',
     img: './assets/images/soupe2.png',
     desc: `Célèbre et savoureuse soupe cambodienne traditionnelle, préparée avec des nouilles de riz, bouillon de porc et poulet et ses garnitures.`,
   },
   {
     title: 'Soupe Boeuf Boulette',
     category: 'Plats',
-    price: '12.50',
+    price: '14.00',
     img: './assets/images/soupe3.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
+    desc: `Dégustez l'authentique soupe de nouilles agrémentée de
+    boulettes de boeuf, accompagnée de coriandre, soja et
+    ciboulette.`,
   },
   {
     title: 'Soupe Nouille Canard',
     category: 'Plats',
     price: '12.50',
     img: './assets/images/soupe4.png',
-    desc: `Célèbre et savoureuse soupe cambodienne traditionnelle, préparée avec des nouilles de riz, bouillon de porc et poulet et ses garnitures.`,
+    desc: `Savoureuse soupe de nouilles fraîches accompagnée de
+    tranches de canard, coriandre et soja.`,
   },
   {
     title: 'Soupe raviolis',
     category: 'Plats',
-    price: '11.50',
+    price: '12.50',
     img: './assets/images/soupe.png',
     desc: `Voyagez en Chine avec notre soupe de raviolis, grâce à son bouillon fait maison, et garnis d'un mélange de viande hachée de porc et de crevettes.`,
   },
   {
-    title: 'Boeuf Loc Lac',
+    title: 'Loc Lac',
     category: 'Plats',
-    price: '11.50',
+    price: '12.50',
     img: './assets/images/loklak.png',
     desc: `De délicieux émincés de boeuf caramélisés sautés au Wok, accompagnés de son riz blanc sauté à la tomate, concombre, oignons et ciboulette.`,
   },
   {
-    title: 'Bo Bun (Boeuf ou Poulet)',
+    title: 'Bo Bun',
     category: 'Plats',
-    price: '11.00',
+    price: '12.00',
     img: './assets/images/bol2.png',
-    desc: `Véritable plat complet et gourmand, composé de nouilles de riz, crudités fraîches et marinées, d'herbes parfumées, garni d'une grillade de viande et sublimé par une sauce acidulée et de nems bien croustillants.`,
+    desc: `Véritable plat complet et gourmand, composé de nouilles de
+    riz, crudités fraîches et marinées, d'herbes parfumées, garni
+    d'une grillade de viande et sublimé par une sauce acidulée et
+    de nems bien croustillants. </br>
+    Choix entre boeuf ou poulet.`,
   },
   {
     title: 'Poke Bowl KHN',
     category: 'Plats',
-    price: '12.50',
+    price: '13.00',
     img: './assets/images/pokebawl.png',
     desc: `Vedette de la cuisine Healthy, le "Poké" KHN vous régalera par sa fraîcheur tout en restant sain. Il se compose de nouilles de riz, avocat, crudités et de généreux morceaux de saumon frais aux parfums asiatiques.`,
   },
   {
     title: 'Banh Sung',
     category: 'Plats',
-    price: '12.00',
+    price: '13.00',
     img: './assets/images/udon.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
+    desc: `Salade de vermicelles cambodgienne et cousine du Bo
+    bun conciliant crudités, nems, le tout arrosé de lait de
+    coco. Simple, rafraîchissante et gourmande!.`,
   },
   {
-    title: 'Lot Tcha (Sans Viande)',
+    title: 'Lot Tcha',
     category: 'Plats',
-    price: '11.50',
+    price: '13.00',
     img: './assets/images/udon2.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
-  },
-  {
-    title: 'Lot Tcha (Tofu)',
-    category: 'Plats',
-    price: '12.00',
-    img: './assets/images/udon2.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
-  },
-  {
-    title: 'Lot Tcha (Boeuf, Poulet ou Crevette)',
-    category: 'Plats',
-    price: '12.50',
-    img: './assets/images/udon2.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
+    desc: `En provenance du Cambodge, de délicates petites
+    nouilles de riz fraîches, sautées avec du chou, soja et
+    servies avec un oeuf sur le plat. Possibilité avec Tofu.
+    Supplément boeuf, poulet, crevettes (+0,50€).`,
   },
   // {
   //   title: 'Soupe',
@@ -142,30 +147,31 @@ const menu = [
   //   desc: `Célèbre et savoureuse soupe cambodienne traditionnelle, préparée avec des nouilles de riz, bouillon de porc et poulet et ses garnitures.`,
   // },
   {
-    title: 'Nouilles Sautées (Sans Viande)',
+    title: 'Nouilles sautées nature',
     category: 'Plats',
-    price: '9.50',
-    img: './assets/images/ramen2.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées !.`,
+    price: '10.50',
+    img: './assets/images/nouillessautees3.png',
+    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.
+    </br>Au choix : boeuf, poulet ou crevettes (supplément : +1,50€) </br>
+    végétarien (supplément : +1,00€) </br>
+    canard (supplément : +3,00€)`,
   },
   {
-    title: 'Nouilles sautées (Boeuf, Poulet ou Crevette)',
+    title: 'Pad Thai',
     category: 'Plats',
-    price: '11.00',
-    img: './assets/images/noodles1.png',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
-  },
-  {
-    title: 'Pad Thai (Boeuf, Poulet ou Crevette)',
-    category: 'Plats',
-    price: '12.50',
-    img: './assets/images/padthai.JPG',
-    desc: `Voici nos fameuses nouilles sautées au Wok moelleuses et parfumées ! A base de boeuf, poulet, crevettes ou full légumes, une bouchée suffira à vous transporter au coeur de l'Asie ; vous aurez le choix entre plusieurs assaisonnements : Soja, Aigre douce, Saté ou Curry.`,
+    price: '13.00',
+    img: './assets/images/padthai.png',
+    desc: `Savourez le fameux Pad Thai, plat traditionnel thaïlandais à la
+    fois rassasiant et diététique. Composé d'une base de nouilles
+    de riz, agrémentés de légumes, de tofu, de viande ou fruits
+    de mer, assaisonné de sauce soja, cacahuètes concassées et
+    fines herbes. </br>
+    Choix entre boeuf, poulet ou crevettes.`,
   },
   {
     title: 'Mango Sticky Rice ',
     category: 'Desserts',
-    price: '6.00',
+    price: '6.50',
     img: './assets/images/mango.jpg',
     desc: `Dessert gourmand originaire du Laos, à base de riz, de lait de coco et de mangue.`,
   },
@@ -173,20 +179,20 @@ const menu = [
     title: 'Boule de neige coco',
     category: 'Desserts',
     price: '5.50',
-    img: './assets/images/boule-coco.jpg',
+    img: './assets/images/perlescoco2.png',
     desc: `Découvrez les réputées boules de coco, préparées avec de la noix de coco et de la farine de riz gluant.`,
   },
   {
     title: 'Salade de fruits',
     category: 'Desserts',
-    price: '4.00',
-    img: './assets/images/salade-fruits.png',
+    price: '4.50',
+    img: './assets/images/saladedefruits.png',
     desc: `Délicieuse salade de fruits fraîche et raffraîchissante.`,
   },
   {
     title: 'Mochi glacé',
     category: 'Desserts',
-    price: '5.50',
+    price: '6.00',
     img: './assets/images/mochi-glace.jpeg',
     desc: `Savourez la glace autrement avec le mochi glacé, spécialité japonaise correspondant à une demi sphère en pâte de riz garnie d'un coeur glacé.`,
   },
@@ -195,7 +201,8 @@ const menu = [
     category: 'Desserts',
     price: '4.00',
     img: './assets/images/glace.png',
-    desc: `Savourez la glace autrement avec le mochi glacé, spécialité japonaise correspondant à une demi sphère en pâte de riz garnie d'un coeur glacé.`,
+    desc: `Fameuses boules glacées (2 boules).
+    Parfums : Vanille, Citron, Chocolat, Fraise, Pistache.`,
   },
 ];
 
